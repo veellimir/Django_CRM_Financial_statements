61f3eeca07b3ad5173d1b4116d0c50c3aa37fda1
vellimir <kkazancov@gmail.com>
2024-04-27 14:30:34 +0400

    edit fiedls update status clear db

--- reports_fintablo/urls.py
+++ reports_fintablo/urls.py
@@ -14,15 +14,12 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('my_report/', views.reports_user, name='user_report'),
 
-    # path('all-reports/', views.all_reports, name='all_reports'),
-    # path('new-report/', views.new_report, name='new_report'),
-    # path('verify-reports/', views.verify_reports, name='verify_reports'),
-
     path('all-reports/', views.all_reports, {'endpoint': 'all_reports'}, name='all_reports'),
     path('new-report/', views.all_reports, {'endpoint': 'new_report'}, name='new_report'),
     path('verify-reports/', views.all_reports, {'endpoint': 'verify_reports'}, name='verify_reports'),
 
     path('verify-report/<int:operation_id>/', views.verify_report, name='verify_report'),
+    path('rejected-report/<int:operation_id>/', views.rejected_report, name='rejected_report'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
