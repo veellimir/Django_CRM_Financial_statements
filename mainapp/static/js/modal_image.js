b1fd37a9a3a277ed80a9116333bec03fa099f293
vellimir <kkazancov@gmail.com>
2024-06-05 18:39:28 +0400

    create comments for rejected reports

--- mainapp/static/js/modal_image.js
+++ mainapp/static/js/modal_image.js
@@ -19,6 +19,69 @@ modalImage.addEventListener('click', function(event) {
 
 document.querySelectorAll(".report_form").forEach(function(form) {
     form.addEventListener("submit", function() {
-            document.querySelector(".modal_loading").style.display = "flex";
+        document.querySelector(".modal_loading").style.display = "flex";
     });
+});
+
+
+// Modal comments
+const btnModalComments = document.querySelectorAll('.modalComments'),
+      modalComments = document.querySelectorAll('.wrapper-modal_comment'),
+      btnCloseModal = document.querySelector('.btn_close_comment');
+
+
+btnModalComments.forEach(function(btn) {
+    btn.addEventListener('click', function () {
+        const commentId = btn.getAttribute('data-comment-id'),
+              modalComment = document.getElementById(commentId);
+        
+        if (modalComment) {
+            modalComment.style.display = 'block';
+        }
+    });
+});
+
+modalComments.forEach(function(modal) {
+    const btnCloseModal = modal.querySelector('.btn_close_comment');
+    if (btnCloseModal) {
+        btnCloseModal.addEventListener('click', function() {
+            modal.style.display = 'none';
+        });
+    }
+});
+
+
+// Modal comments
+document.addEventListener('DOMContentLoaded', function() {
+    const rejectButtons = document.querySelectorAll('.btn__valid_reports');
+
+    rejectButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            const operationId = button.getAttribute('data-operation-id');
+            const modal = document.getElementById(`modal-comment-${operationId}`);
+            if (modal) {
+                modal.style.display = 'flex';
+            }
+        });
+    });
+
+    const closeModalButtons = document.querySelectorAll('.close-modal');
+
+    closeModalButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            const modal = button.closest('.wrapper-comment');
+            if (modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
+
+    window.onclick = function(event) {
+        const modals = document.querySelectorAll('.wrapper-comment');
+        modals.forEach(function(modal) {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+            }
+        });
+    }
 });
